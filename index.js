75b06080be41f7ef1f0e43133aaaa83d21b79514
Rene Funcion <rcfun959@gmail.com>
2023-06-15 03:22:20 -0700

    Completed phase-1-sending-data-with-fetch

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Add your code here
+function submitData(name, email) {
+  let data = {
+    name: name,
+    email: email,
+  };
+
+  return fetch("http://localhost:3000/users", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify(data),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      let messageDiv = document.getElementById("message");
+      let newText = document.createTextNode(`New User ID: ${data.id}`);
+      messageDiv.appendChild(newText);
+    })
+    .catch((error) => {
+      let messageDiv = document.getElementById("message");
+      let newText = document.createTextNode(
+        `An error occurred: ${error.message}`
+      );
+      messageDiv.appendChild(newText);
+    });
+}
